00f448701ce00a0fa47dac4f1f1d33602a66cbe5
SterrenHeer <sterrenheer@gmail.com>
2022-01-06 14:16:23 +0300

    Add models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from core.models import Person, Course, Grade
 
-# Register your models here.
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    pass
--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Person(models.Model):
+    last_name = models.TextField()
+    first_name = models.TextField()
+    courses = models.ManyToManyField("Course", blank=True)
+
+    class Meta:
+        verbose_name_plural = "People"
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Course(models.Model):
+    name = models.TextField()
+    year = models.IntegerField()
+
+    class Meta:
+        unique_together = ("name", "year", )
+
+    def __str__(self):
+        return self.name + ' (' + str(self.year) + ')'
+
+
+class Grade(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    grade = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)])
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.person) + ' ' + str(self.course)
